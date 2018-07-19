b8acc0f06618e5415f7607d212a049a9988cb357
sephirothkod <bjustice@uvic.ca>
2018-07-19 10:47:25 -0700

    removes reliance on secondary_terms method in Hyrax forms. Allows for a custom, Default Work, specified in the yaml. Removes reliance on Hyrax for generating CSV's from ContentDM. Fixes issue where CSV outputs GenericWork instead of the chosen work.

--- app/controllers/cdm_migrator/cdm_controller.rb
+++ app/controllers/cdm_migrator/cdm_controller.rb
@@ -5,27 +5,12 @@ module CdmMigrator
 			super
 			@cdm_url = CdmMigrator::Engine.config["cdm_url"]
 			@cdm_port = CdmMigrator::Engine.config["cdm_port"]
+			@default_fields = CdmMigrator::Engine.config["default_fields"]
 			@cdm_dirs = CdmMigrator::Engine.config["cdm_dirs"] || false
 		end
-		
+
 		before_action :set_exclusive_fields, only: [:generate, :mappings]
-		
-		def secondary_terms file_form
-        file_form.terms - file_form.required_fields -
-          [:visibility_during_embargo, :embargo_release_date,
-           :visibility_after_embargo, :visibility_during_lease,
-           :lease_expiration_date, :visibility_after_lease, :visibility,
-           :thumbnail_id, :representative_id, :ordered_member_ids,
-           :collection_ids, :in_works_ids, :admin_set_id]
-      end
-		
-		def set_exclusive_fields
-		  #Module.const_get "Hyrax::GenericWorkForm" rescue false #.split('::').inject(Object) {|o,c| o.const_get c}
-		  file_form = Module.const_get("Hyrax::FileSetForm") rescue nil || Module.const_get("Hyrax::Forms::FileSetEditForm")
-		  work_form = Module.const_get("Hyrax::#{params[:work]}Form") rescue nil || Module.const_get("Hyrax::Forms::WorkForm")
-			@terms = file_form.required_fields + secondary_terms(file_form)
-			@work_only = work_form.required_fields + work_form.new(params[:work].constantize.new,nil,nil).secondary_terms - @terms
-		end
+
 
 		def generate
 			@h_to_c = {}
@@ -59,7 +44,7 @@ module CdmMigrator
 			records.each do |rec|
 				if rec.last == "cpd"
 					json = JSON.parse(Net::HTTP.get_response(URI.parse("#{@cdm_url}:#{@cdm_port}/dmwebservices/index.php?q=dmGetItemInfo/#{params[:collection]}/#{rec.first}/json")).body)
-					csv_lines << create_line("GenericWork","",json)
+					csv_lines << create_line(params[:work],"",json)
 					json = JSON.parse(Net::HTTP.get_response(URI.parse("#{@cdm_url}:#{@cdm_port}/dmwebservices/index.php?q=dmGetCompoundObjectInfo/#{params[:collection]}/#{rec.first}/json")).body)
 					rec_pages = json['page'] || json['node']['page']
 					rec_pages.each do |child|
@@ -70,7 +55,7 @@ module CdmMigrator
 					end
 				else
 					json = JSON.parse(Net::HTTP.get_response(URI.parse("#{@cdm_url}:#{@cdm_port}/dmwebservices/index.php?q=dmGetItemInfo/#{params[:collection]}/#{rec.first}/json")).body)
-					csv_lines << create_line("GenericWork","",json)
+					csv_lines << create_line(params[:work],"",json)
 					url = "file://#{file_path(rec.first)}"
 					url = "#{@cdm_url}/utils/getfile/collection/#{params[:collection]}/id/#{rec.first}/filename/#{rec.first}.#{rec.last}" unless params[:file_system]=="true" #"file://#{file_path(rec.first)}"
 					csv_lines << create_line("File",url,{})
@@ -91,49 +76,90 @@ module CdmMigrator
 		def collection
 			json = JSON.parse(Net::HTTP.get_response(URI.parse("#{@cdm_url}:#{@cdm_port}/dmwebservices/index.php?q=dmGetCollectionList/json")).body)
 			@collections = json.collect { |c| [c['name'],c['secondary_alias']] }
-			@available_concerns = Hyrax.config.curation_concerns.map { |c| [c.to_s, c.to_s]}
+			load_concerns
 		end
 
 		protected
 
-			def create_line type, url, json
-				line = [] << type
-				line << url
-				(@terms+@work_only).each do |term|
-					content = []
-					unless @h_to_c[term.to_s].nil?
-						@h_to_c[term.to_s].each do |cdm_term|
-							content << json[cdm_term] unless json[cdm_term].nil?
-						end
-						content.delete_if(&:empty?)
-					end
-					if content.nil? || content.empty? || content == [{}]
-						line << ""
-					else
-						line << content.join('|')
-					end
-				end
-				CSV.generate_line line
-			end
+		def standalone
+			Hyrax rescue nil
+		end
 
-			def file_path pointer
-				file_types = ['tif','jpg','mp4','mp3']
-				files = []
-				file_types.each do |type|
-					files << Dir.glob("#{params['mappings_url']}/**/#{pointer}_*#{type}")
-				end
-				files.each do |file|
-					return file.first if file.count > 0
-				end
+		def load_concerns
+			@available_concerns = []
+			unless @default_fields.nil?
+				@available_concerns += [['DefaultWork', 'DefaultWork']]
 			end
+			unless standalone.nil?
+				@available_concerns += Hyrax.config.curation_concerns.map { |c| [c.to_s, c.to_s]}
+			end
+		end
 
-			def get_dirs
-			  @dirs = []
-			  @cdm_dirs.each do |name, dir|
-			    ent = Dir.entries(dir).select {|entry| File.directory? File.join(dir,entry) and !(entry =='.' || entry == '..') }
-			    ent = ent.map { |url| ["#{name}/#{url}", "#{dir}/#{url}"] }
-			    @dirs += ent
+		def work_form
+			Module.const_get("Hyrax::#{params[:work]}Form") rescue nil || Module.const_get("Hyrax::Forms::WorkForm")
+		end
+
+		def file_form
+			Module.const_get("Hyrax::FileSetForm") rescue nil || Module.const_get("Hyrax::Forms::FileSetEditForm")
+		end
+
+		def secondary_terms form_name
+			form_name.terms - form_name.required_fields -
+					[:visibility_during_embargo, :embargo_release_date,
+					 :visibility_after_embargo, :visibility_during_lease,
+					 :lease_expiration_date, :visibility_after_lease, :visibility,
+					 :thumbnail_id, :representative_id, :ordered_member_ids,
+					 :collection_ids, :in_works_ids, :admin_set_id, :files, :source, :member_of_collection_ids]
+		end
+
+		def set_exclusive_fields
+			if params[:work] != 'DefaultWork'
+				@terms = file_form.required_fields + secondary_terms(file_form)
+				@work_only = (secondary_terms work_form) - @terms
+			else
+				@terms = @default_fields
+				@work_only = []
+			end
+		end
+
+		def create_line type, url, json
+			line = [] << type
+			line << url
+			(@terms+@work_only).each do |term|
+				content = []
+				unless @h_to_c[term.to_s].nil?
+					@h_to_c[term.to_s].each do |cdm_term|
+						content << json[cdm_term] unless json[cdm_term].nil?
+					end
+					content.delete_if(&:empty?)
+				end
+				if content.nil? || content.empty? || content == [{}]
+					line << ""
+				else
+					line << content.join('|')
 				end
 			end
+			CSV.generate_line line
+		end
+
+		def file_path pointer
+			file_types = ['tif','jpg','mp4','mp3']
+			files = []
+			file_types.each do |type|
+				files << Dir.glob("#{params['mappings_url']}/**/#{pointer}_*#{type}")
+			end
+			files.each do |file|
+				return file.first if file.count > 0
+			end
+		end
+
+		def get_dirs
+			@dirs = []
+			@cdm_dirs.each do |name, dir|
+				ent = Dir.entries(dir).select {|entry| File.directory? File.join(dir,entry) and !(entry =='.' || entry == '..') }
+				ent = ent.map { |url| ["#{name}/#{url}", "#{dir}/#{url}"] }
+				@dirs += ent
+			end
+		end
 	end
 end
