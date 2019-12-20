b2212ed0c8f67da04e6b96384c2815585e4b8442
sephirothkod <bjustice@uvic.ca>
2019-12-20 07:58:46 -0800

    added localization to the controller so that the locale persists

--- app/controllers/cdm_migrator/application_controller.rb
+++ app/controllers/cdm_migrator/application_controller.rb
@@ -1,5 +1,10 @@
 module CdmMigrator
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
   end
 end
