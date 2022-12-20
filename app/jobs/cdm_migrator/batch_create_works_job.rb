30f7bb056e2fd71e9fddb0d7cebca5b0b947dde1
Tiffany Chan <tjychan@uvic.ca>
2022-12-19 16:20:46 -0800

    Make gem compatible with Rails 5.2.6

--- app/jobs/cdm_migrator/batch_create_works_job.rb
+++ app/jobs/cdm_migrator/batch_create_works_job.rb
@@ -4,8 +4,14 @@ module CdmMigrator
 
     def perform(ingest, user)
       ingest.data.each do |w|
-        ingest_work = IngestWork.new(w, ingest.id)
+        ingest_work = IngestWork.new({
+           work_type: w[:type],
+           data: w[:metadata],
+           files: w[:files],
+           batch_ingest_id: ingest.id
+        })
         ingest_work.save!
+        # puts ingest_work.inspect
         CreateWorkJob.perform_later ingest_work, user, ingest.admin_set_id, ingest.collection_id
       end
 
--- app/models/cdm_migrator/ingest_work.rb
+++ app/models/cdm_migrator/ingest_work.rb
@@ -1,14 +1,5 @@
 module CdmMigrator
-  class IngestWork < ActiveRecord::Base
-
-    def initialize(work, ingest_id)
-      super({
-        work_type: work[:type],
-        data: work[:metadata],
-        files: work[:files],
-        batch_ingest_id: ingest_id
-            })
-    end
+  class IngestWork < ApplicationRecord
 
     serialize :data
     serialize :files
