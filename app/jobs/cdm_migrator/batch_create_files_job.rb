461584a6cfdc28636d314380c29ec6fbf9ce58d6
Tiffany Chan <tjychan@uvic.ca>
2023-01-27 14:15:44 -0800

    Make BatchCreateFilesJob idempotent.

--- app/jobs/cdm_migrator/batch_create_files_job.rb
+++ app/jobs/cdm_migrator/batch_create_files_job.rb
@@ -3,13 +3,25 @@ module CdmMigrator
     queue_as Hyrax.config.ingest_queue_name
 
     def perform work, ingest_work, user
-      ingest_work.files.each do |file|
+      unless work.ordered_members.to_a.empty?
+        # This only executes if the job failed before adding any file sets and is now retrying
+        ordered_members = work.ordered_members.to_a
+        # If the last file set's file was never ingested, do that now
+        last_file_set = ordered_members.last
+        if last_file_set.present? and last_file_set.files.none?
+          CdmMigrator::CdmIngestFilesJob.perform_later(last_file_set, last_file_set.import_url, user)
+        end
+        files = ingest_work.files[ordered_members.length..-1]
+      else
+        files = ingest_work.files
+      end
+      files.each do |file|
         url = file[:url]
         last_file = ingest_work.files.last==file
         ::FileSet.new(import_url: url, label: file[:title]) do |fs|
           fs.save
           actor = Hyrax::Actors::FileSetActor.new(fs, user)
-          actor.create_metadata#(work, visibility: work.visibility)
+          actor.create_metadata
           actor.attach_file_to_work(work)
           fs.attributes = file[:metadata]
           fs.save!
