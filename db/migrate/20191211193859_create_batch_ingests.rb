604b8d3c838b878733e2f2c8d57a888a1e293891
Tiffany Chan <tjychan@uvic.ca>
2023-06-26 11:05:32 -0700

    Prevent Duplicate Table error on rails db:migrate and bump version.

--- db/migrate/20191211193859_create_batch_ingests.rb
+++ db/migrate/20191211193859_create_batch_ingests.rb
@@ -1,16 +1,18 @@
 class CreateBatchIngests < ActiveRecord::Migration[5.0]
   def up
-    create_table :batch_ingests do |t|
-      t.text :data
-      t.string :admin_set_id
-      t.string :collection_id
-      t.text :message
-      t.integer :size
-      t.string :csv
-      t.references :user, foreign_key: true
-      t.boolean :complete, default: false
+    unless table_exists?(:batch_ingests)
+      create_table :batch_ingests do |t|
+        t.text :data
+        t.string :admin_set_id
+        t.string :collection_id
+        t.text :message
+        t.integer :size
+        t.string :csv
+        t.references :user, foreign_key: true
+        t.boolean :complete, default: false
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 
