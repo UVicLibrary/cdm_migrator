8488aecc27cb2e5082f722887c5dc691e1cba831
Tiffany Chan <tjychan@uvic.ca>
2025-03-12 12:53:25 -0700

    Check if the ingest_works table already exists before trying to create it.

--- db/migrate/20191212192315_create_ingest_works.rb
+++ db/migrate/20191212192315_create_ingest_works.rb
@@ -1,14 +1,16 @@
 class CreateIngestWorks < ActiveRecord::Migration[5.0]
   def up
-    create_table :ingest_works do |t|
-      t.string :work_type
-      t.text :data
-      t.text :files
-      t.boolean :complete, :default => false
+    unless table_exists?(:ingest_works)
+      create_table :ingest_works do |t|
+        t.string :work_type
+        t.text :data
+        t.text :files
+        t.boolean :complete, :default => false
 
-      t.references :batch_ingest, foreign_key: true
+        t.references :batch_ingest, foreign_key: true
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 
